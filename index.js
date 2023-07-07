cc3783bcef8254e0e3179e9dd4b05c3f818b7e4a
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-07-07 10:58:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.get('/', (req, res) => {
     <html>
     <head>
       <title>News API</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     </head>
     <body>

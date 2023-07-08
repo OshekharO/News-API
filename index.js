40d64c8a845d78ad81429973c196b335e228b9cd
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-07-08 14:00:58 +0530

    Update base

--- index.js
+++ index.js
@@ -79,6 +79,8 @@ body {
 <p>Your one-stop solution to access the latest news, search for torrents, get insights from Genius, fetch data from Newscatcher, and more.</p>
 <h3>Base URL</h3>
 <p>https://news-api-six-navy.vercel.app</p>
+<p>https://news-api-czsp.onrender.com</p>
+<p>https://news-api.cyclic.app</p>
 <div class="separator"></div>
 <h6>API Endpoints</h6>
 

dd96b062bc049efb9eebec8d1ab38249e4f256c3
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-07-20 18:18:32 +0530

    Update tikmate.js

--- scraper/tikmate.js
+++ scraper/tikmate.js
@@ -1,16 +1,18 @@
-// tikmate.js
+// in tikmate.js
 const axios = require('axios');
 
-exports.getInfo = async function(url) {
-  try {
-    const { data } = await axios.post('https://api.tikmate.app/api/lookup', { url }, { headers: { referer: 'https://tikmate.app' } });
-    
+async function tikmateApp(url) {
+    const { data } = await axios.post('https://api.tikmate.app/api/lookup',
+        { url: url },
+        {
+            headers: {
+                referer: 'https://tikmate.app',
+                'Content-Type': 'application/json',
+            },
+        })
     return {
-      ...data,
-      videoUrl: `https://tikmate.app/download/${data.token}/${data.id}.mp4`,
-      videoUrlHd: `https://tikmate.app/download/${data.token}/${data.id}.mp4?hd=1`,
-    };
-  } catch (error) {
-    throw error;
-  }
-};
+        ...data,
+        videoUrl: `https://tikmate.app/download/${data.token}/${data.id}.mp4`,
+        videoUrlHd: `https://tikmate.app/download/${data.token}/${data.id}.mp4?hd=1`,
+    }
+}

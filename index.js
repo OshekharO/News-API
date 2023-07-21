b1a57a38a463ee0b85e05c2d2d4ba50cce32d1ca
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-07-22 01:20:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { torrent1337x } = require('./scraper/1337x');
 const { nyaaSI } = require('./scraper/nyaaSI');
 const { yts } = require('./scraper/yts');
 const peakpx = require('./scraper/peakpx');
+const scrapePixiv = require('./scraper/pixiv');
 // API keys for newsapi.org
 const API_KEYS_NEWSAPI = ['cab817200f92426bacb4edd2373e82ef', '429904aa01f54a39a278a406acf50070', '28679d41d4454bffaf6a4f40d4b024cc', 'd9903836bbca401a856602f403802521', 'badecbdafe6a4be6a94086f2adfa9c06', '5fbf109857964643b73a2bc2540b36b6'];
 let currentKeyIndexNewsApi = 0;
@@ -207,6 +208,16 @@ app.get('/api/peakpx/:query/:page?', async (req, res) => {
             console.error('Error:', error);
             res.status(500).json({ message: 'An error occurred while fetching peakpx images.' });
         });
+});
+
+app.get('/api/pixiv', async (req, res) => {
+    try {
+        const results = await scrapePixiv();
+        res.json(results);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Server error' });
+    }
 });
 
 app.get('/api/slok/:ch?/:sl?', async (req, res) => {

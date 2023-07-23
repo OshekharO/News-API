233d895664f67830450997d18985d154bc197889
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-07-23 09:30:01 +0530

    Update radio.js

--- scraper/radio.js
+++ scraper/radio.js
@@ -1,22 +1,19 @@
 const axios = require('axios');
-const xml2js = require('xml2js');
 
 async function getStations(page = 1) {
-  const perPage = 10;
-  const url = `http://de1.api.radio-browser.info/xml/stations/topvote/${(page - 1) * perPage}/${perPage}`;
-
   try {
-    const response = await axios.get(url);
-    const result = await xml2js.parseStringPromise(response.data, { mergeAttrs: true });
-    const stations = result.stationlist.station.map(station => ({
-      name: station.name[0],
-      url_resolved: station.url_resolved[0],
-      country: station.country[0],
-      language: station.language[0],
+    const { data } = await axios.get('http://de1.api.radio-browser.info/json/stations/topvote');
+    const start = (page - 1) * 10;
+    const end = start + 10;
+    const stationsPage = data.slice(start, end);
+    return stationsPage.map(station => ({
+      name: station.name,
+      url: station.url_resolved,
+      country: station.country,
+      language: station.language,
     }));
-    return stations;
   } catch (error) {
-    throw new Error(`Failed to fetch stations: ${error.message}`);
+    throw error;
   }
 }
 

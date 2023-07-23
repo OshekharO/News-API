c30aaaf72c216b1ea6010aeb2112e4934094aea6
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2023-07-23 08:11:46 +0530

    Create giphy.js

---
+++ scraper/giphy.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+const apiKey = 'cV0tlNG65o5QnWVuGhhVMafKo3BpeeHT';
+
+async function getGifs(searchQuery, page = 1) {
+  const limit = 10;
+  const offset = (page - 1) * limit;
+
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(searchQuery)}&limit=${limit}&offset=${offset}`;
+  
+  try {
+    const response = await axios.get(url);
+    const gifs = response.data.data.map(gif => ({
+      title: gif.title,
+      images: {
+        gif_url: gif.images.original.url.replace('media4.giphy.com', 'i.giphy.com'),
+        mp4: gif.images.original.mp4.replace('media4.giphy.com', 'i.giphy.com'),
+  }
+    }));
+    return gifs;
+  } catch (error) {
+    throw new Error(`Failed to fetch GIFs: ${error.message}`);
+  }
+}
+
+module.exports = getGifs;

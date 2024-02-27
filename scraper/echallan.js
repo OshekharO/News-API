727cf0e4c33f64d3b223ed2632ab256ae1670e55
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2024-02-27 09:02:03 +0530

    Update echallan.js

--- scraper/echallan.js
+++ scraper/echallan.js
@@ -7,26 +7,37 @@ exports.traffic = function (challanNumber) {
       .get(`https://echallan.parivahan.gov.in/api/challan-status/challan/${challanNumber}`)
       .then((res) => {
         const $ = cheerio.load(res.data);
-        const extractedData = {};
 
+        let extractedData = {};
+        
+        // Extract data under h5 headers
         $("h5").each((index, element) => {
           const key = $(element).text().trim();
           const tableData = [];
 
-          $(element)
-            .next("table")
-            .find("tbody tr")
-            .each((i, row) => {
-              const rowData = {};
-              $(row)
-                .find("td")
-                .each((j, cell) => {
-                  const header = $(cell).attr("scope");
-                  const value = $(cell).text().trim();
-                  rowData[header] = value;
-                });
-              tableData.push(rowData);
-            });
+          if ($(element).parent().is("div")) {
+            // Check if there's a table below the heading
+            const tableElement = $(element).parentsUntil("div.row").last().find("table");
+            if (tableElement.length > 0) {
+              tableElement.find("tbody tr").each((i, row) => {
+                const rowData = {};
+                $(row)
+                  .find("td")
+                  .each((j, cell) => {
+                    const header = $(cell).attr("scope");
+                    const value = $(cell).text().trim();
+                    rowData[header] = value;
+                  });
+                tableData.push(rowData);
+              });
+            } else {
+              // If no table found, use empty array as default
+              tableData = [];
+            }
+          } else {
+            // Ignore other elements without tables
+            tableData = [];
+          }
 
           extractedData[key] = tableData;
         });
